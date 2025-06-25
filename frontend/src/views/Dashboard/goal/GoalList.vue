203a2aab5bce6476df9a6c4dd3c6d7d3846c5e9b
Leishangthem Santosh <santoshleishangthem1997@gmail.com>
2025-06-25 20:46:49 +0530

    UI

--- frontend/src/views/Dashboard/goal/GoalList.vue
+++ frontend/src/views/Dashboard/goal/GoalList.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <a-button type="primary" @click="showModal">
-      Open Modal with customized footer
-    </a-button>
+    <a-button type="primary" @click="showModal"> Create Goal </a-button>
 
     <a-modal v-model:open="open" title="Create Goal">
       <div>
@@ -69,29 +67,20 @@
     :data-source="listData"
   >
     <template #renderItem="{ item }">
-      <a-list-item :key="item.title">
-        <template #actions>
-          <span v-for="{ icon, text } in actions" :key="icon.name">
-            <component :is="icon" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-        </template>
+      <a-list-item :key="item.goal_id">
         <template #extra>
-          <img
-            width="272"
-            alt="logo"
-            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-          />
+          <a-button type="primary" size="small">
+            <template #icon>
+              <EditFilled />
+            </template>
+          </a-button>
         </template>
         <a-list-item-meta :description="item.description">
           <template #title>
             <a :href="item.href">{{ item.title }}</a>
           </template>
-          <template #avatar>
-            <a-avatar :src="item.avatar" />
-          </template>
         </a-list-item-meta>
-        {{ item.content }}
+        <!-- {{ item.content }} -->
       </a-list-item>
     </template>
   </a-list>
@@ -104,6 +93,8 @@ import {
   StarOutlined,
   LikeOutlined,
   MessageOutlined,
+  EditOutlined,
+  EditFilled,
 } from "@ant-design/icons-vue";
 
 // Breadcrumb
@@ -120,18 +111,29 @@ async function fetchData() {
   try {
     const url = import.meta.env.VITE_API_BASE_URL + "goals";
     const response = await axios.get(url);
-    // Dummy data for testing
-    for (let i = 0; i < 5; i++) {
+    const data = response.data.data;
+
+    data.map((goal) => {
       listData.value.push({
-        href: "https://www.antdv.com/",
-        title: `ant design vue part ${i}`,
-        avatar: "https://joeschmoe.io/api/v1/random",
-        description:
-          "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-        content:
-          "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+        goal_id: goal.goal_id,
+        href: "goals/" + goal.goal_id,
+        title: goal.goal_title,
+        description: goal.goal_description,
       });
-    }
+    });
+
+    // // Dummy data for testing
+    // for (let i = 0; i < 5; i++) {
+    //   listData.value.push({
+    //     href: "https://www.antdv.com/",
+    //     title: `ant design vue part ${i}`,
+    //     avatar: "https://joeschmoe.io/api/v1/random",
+    //     description:
+    //       "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    //     content:
+    //       "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+    //   });
+    // }
   } catch (err) {
     console.error("Failed to fetch goals:", err);
   }
